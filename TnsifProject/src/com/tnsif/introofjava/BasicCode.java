19dfb72d3ca09c60a4b7dd18afc87b522132f3eb
suriya sri <suriyasricse@gmail.com>
2026-01-08 12:35:13 +0530

    1st Commit

---
+++ TnsifProject/src/com/tnsif/introofjava/BasicCode.java
@@ -0,0 +1,30 @@
+package com.tnsif.introofjava;
+
+public class BasicCode {
+
+	public static void main(String[] args) {
+		
+		int age = 21;
+		double salary = 50000.00;
+		boolean isPassed = true;
+		char grade = 'A';
+		
+		
+		double newSalary = age; 
+		
+		int roundSalary = (int)salary; 
+		
+		
+		int increment = 5000;
+		
+		int total = roundSalary + increment;
+		
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(roundSalary);
+		System.out.println(total);
+
+	}
+
+}
