72069593bec51743f12dd7172754405de4826ad5
suriya sri <suriyasricse@gmail.com>
2026-01-09 21:05:25 +0530

    wrapper commit

---
+++ TnsifProject/src/Interface/Wrapper.java
@@ -0,0 +1,18 @@
+package Interface;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		
+		Integer intObj = Integer.valueOf(5);
+        Double doubleObj = Double.valueOf(5.99);
+        Character charObj = Character.valueOf('A');
+        Boolean boolObj = Boolean.valueOf(true);
+        
+        System.out.println(intObj);      
+        System.out.println(doubleObj);  
+        System.out.println(charObj);
+
+	}
+
+}
